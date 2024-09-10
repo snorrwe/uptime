924cc5712bcf4807d930f48e60a56b3db22df54b
Daniel Kiss <littlesnorrboy@gmail.com>
2024-09-10 22:33:25 +0200

    Add logger

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,5 @@
 let () = print_endline "Hello, World!"
 
 let () =
-  Dream.run (fun _ ->
-    Dream.html "Good morning, world!")
+  Dream.run (Dream.logger(fun _ ->
+    Dream.html "Good morning, world!"))
