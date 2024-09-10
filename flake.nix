16f9f9f1aa0644d4172fe91df4519a89dd237c6b
Daniel Kiss <littlesnorrboy@gmail.com>
2024-09-10 22:10:02 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "devshell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            ocamlformat_0_22_4
+            ocaml
+            opam
+          ];
+          shellHook = ''
+            eval $(opam env)
+        '';
+        };
+      }
+    );
+}
