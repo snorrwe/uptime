af8b4ac36aa03602b28214730cf3ad0fa1318e3c
Daniel Kiss <littlesnorrboy@gmail.com>
2024-09-10 22:53:18 +0200

    Add template

--- bin/main.ml
+++ bin/main.ml
@@ -3,5 +3,5 @@ let () =
     @@ Dream.logger
     @@ Dream.router [
         Dream.get "/" (fun _ -> Dream.html "Good morning, world!");
-        Dream.get "/hello/:name" (fun req -> "hello " ^ Dream.param req "name" |> Dream.html);
+        Dream.get "/hello/:name" (fun req -> Dream.param req "name" |> Foo.render |> Dream.html);
     ]
