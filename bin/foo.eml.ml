2e99fe0bf04b983afc9f1fc5287fb0467081b615
Daniel Kiss <littlesnorrboy@gmail.com>
2024-09-11 10:13:10 +0200

    Add htmx

--- bin/foo.eml.ml
+++ bin/foo.eml.ml
@@ -1,5 +1,11 @@
 let render param =
   <html>
+  <head>
+      <script src="https://unpkg.com/htmx.org@2.0.2"
+              integrity="sha384-Y7hw+L/jvKeWIRRkqWYfPcvVxHzVzn5REgzbawhxAuQGwX1XWe70vji+VSeHOThJ"
+              crossorigin="anonymous">
+      </script>
+  </head>
   <body>
     <h1>The URL parameter was <%s param %>!</h1>
   </body>
