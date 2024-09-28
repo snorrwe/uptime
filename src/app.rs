fc919791e46522829a5ed40ce4db38cfc3acdd38
Daniel Kiss <littlesnorrboy@gmail.com>
2024-09-28 18:23:02 +0200

    Fix status list query

--- src/app.rs
+++ src/app.rs
@@ -35,21 +35,24 @@ async fn list_statuses() -> Result<Vec<StatusRow>, ServerFnError> {
     sqlx::query_as!(
         StatusRow,
         r#"
-select
-    se.id,
-    public_url as "public_url!",
-    se."name" as "name!",
-    status_code as "last_status!",
-    sh."created" as "last_update!"
-from status_entry as se
-inner join
-    (
-        select status_id, status_code, created
-        from status_history
-        order by created desc
-        limit 5
-    ) as sh
-    on sh.status_id = se.id
+with
+    ranked_history as (
+        select
+            se.id,
+            public_url as "public_url!",
+            se."name" as "name!",
+            status_code as "last_status!",
+            sh."created" as "last_update!",
+            row_number() over (partition by se.id order by sh.created desc) as rn
+        from status_entry as se
+        inner join
+            (select status_id, status_code, created from status_history) as sh
+            on sh.status_id = se.id
+    )
+select id, "public_url!", "name!", "last_status!", "last_update!"
+
+from ranked_history
+where rn <= 5
 "#
     )
     .fetch_all(db)
