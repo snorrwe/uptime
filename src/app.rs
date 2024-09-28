57b5ca7cb9d4a2e57f0fde2d4377bc6ebbdc9478
Daniel Kiss <littlesnorrboy@gmail.com>
2024-09-28 15:30:29 +0200

    Styling

--- src/app.rs
+++ src/app.rs
@@ -16,7 +16,7 @@ pub fn App() -> impl IntoView {
         <Stylesheet id="leptos" href="/pkg/dashboard.css"/>
 
         // sets the document title
-        <Title text="Welcome to Leptos"/>
+        <Title text="Dashboard"/>
 
         // content for this welcome page
         <Router fallback=|| {
@@ -39,12 +39,7 @@ pub fn App() -> impl IntoView {
 /// Renders the home page of your application.
 #[component]
 fn HomePage() -> impl IntoView {
-    // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
-
     view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        <h1 class="text-xl">Dashboard</h1>
     }
 }
