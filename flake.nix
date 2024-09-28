dda915f3b500992d8ff141d38ca525452183a517
Daniel Kiss <littlesnorrboy@gmail.com>
2024-09-28 14:43:03 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Rust devshell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            just
+            (rust-bin.nightly.latest.default.override {
+              extensions = [ "rust-src" "rust-analyzer" ];
+              targets = [ "wasm32-unknown-unknown" ];
+            })
+            cargo-edit
+            cargo-leptos
+            cargo-generate
+          ];
+        };
+      }
+    );
+}
