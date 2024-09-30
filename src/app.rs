9dbe9f412b88d176621dab56ec996fdf7550f1ed
Daniel Kiss <littlesnorrboy@gmail.com>
2024-09-30 22:08:39 +0200

    Add a spinner

--- src/app.rs
+++ src/app.rs
@@ -113,7 +113,15 @@ fn HomePage() -> impl IntoView {
     view! {
         <h1 class="text-xl">Dashboard</h1>
         <Suspense fallback=move || {
-            view! { <p>"Loading..."</p> }
+            view! {
+                <div
+                    class="animate-spin inline-block size-6 border-[3px] border-current border-t-transparent text-blue-600 rounded-full dark:text-blue-500"
+                    role="status"
+                    aria-label="loading"
+                >
+                    <span class="sr-only">Loading...</span>
+                </div>
+            }
         }>
             {move || {
                 statuses
