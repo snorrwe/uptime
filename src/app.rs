d24da070ea62246563c2466e4a08c1546bd8f9d7
Daniel Kiss <littlesnorrboy@gmail.com>
2024-10-01 21:49:55 +0200

    Refactor

--- src/app.rs
+++ src/app.rs
@@ -153,25 +153,22 @@ fn status_li(s: &[StatusRow]) -> impl IntoView {
                 {&first.name}
             </a>
             " = "
-            <ul class="flex flex-row-reverse gap-1">
-                {s
-                    .iter()
-                    .map(|s| {
-                        view! {
-                            <li class="hs-tooltip [--trigger:hover] inline-block">
+            <ul class="flex flex-row-reverse gap-1">{s.iter().map(status_pip).collect_view()}</ul>
+        </li>
+    }
+}
 
-                                {s.last_status}
-                                <span
-                                    class="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-3 px-4 bg-white border text-sm text-gray-600 rounded-lg shadow-md dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400"
-                                    role="tooltip"
-                                >
-                                    {s.poll_time.to_string()}
-                                </span>
-                            </li>
-                        }
-                    })
-                    .collect_view()}
-            </ul>
+fn status_pip(s: &StatusRow) -> impl IntoView {
+    view! {
+        <li class="hs-tooltip [--trigger:hover] inline-block">
+
+            {s.last_status}
+            <span
+                class="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-3 px-4 bg-white border text-sm text-gray-600 rounded-lg shadow-md dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400"
+                role="tooltip"
+            >
+                {s.poll_time.to_string()}
+            </span>
         </li>
     }
 }
