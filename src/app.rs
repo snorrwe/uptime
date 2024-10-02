9504be0a640b7fd196fbe6fd2f1b9ec45dc43c5b
Daniel Kiss <littlesnorrboy@gmail.com>
2024-10-02 11:09:01 +0200

    Use title instead of preline tooltip
    The preline tooltip keeps getting stuck for no apparent reason

--- src/app.rs
+++ src/app.rs
@@ -203,19 +203,12 @@ fn status_pip(s: &StatusRow) -> impl IntoView {
     };
 
     view! {
-        <li class=format!(
-            "{color} hs-tooltip [--trigger:hover] inline-block",
-        )>
-        <span class="cursor-default text-lg hover:text-3xl">
-            {PIP}
-        </span>
+        <li class=color>
             <span
-            class="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-3 px-4 bg-white border text-sm text-gray-600 rounded-lg shadow-md dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400"
-                role="tooltip"
+                class="cursor-default text-lg hover:text-3xl"
+                title=format!("{} Status: {}", s.poll_time.to_string(), s.last_status)
             >
-                {s.poll_time.to_string()}
-                " Status: "
-                {s.last_status}
+                {PIP}
             </span>
         </li>
     }
