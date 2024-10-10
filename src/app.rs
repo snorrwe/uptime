5fa7d3cf2d747177e843c88bafdc85149e93bc82
Daniel Kiss <littlesnorrboy@gmail.com>
2024-10-10 09:52:07 +0200

    Add breadcrumbs

--- src/app.rs
+++ src/app.rs
@@ -156,6 +156,7 @@ pub fn App() -> impl IntoView {
             outside_errors.insert_with_default_key(AppError::NotFound);
             view! { <ErrorTemplate outside_errors /> }.into_view()
         }>
+            <Breadcrumbs />
             <main class="container mx-auto">
                 <Routes>
                     <Route path="" view=HomePage />
@@ -163,6 +164,82 @@ pub fn App() -> impl IntoView {
                 </Routes>
             </main>
         </Router>
+    }
+}
+
+#[component]
+fn BreadcrumbItem(name: String, url: String) -> impl IntoView {
+    view! {
+        <div class="flex items-center">
+            <svg
+                class="rtl:rotate-180 block w-3 h-3 mx-1 text-gray-400 "
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+            >
+                <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m1 9 4-4-4-4"
+                />
+            </svg>
+            <A
+                href=url
+                class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white"
+            >
+                {name}
+            </A>
+        </div>
+    }
+}
+
+#[component]
+fn Breadcrumbs() -> impl IntoView {
+    let path = use_location().pathname;
+    view! {
+        <nav
+            class="flex px-5 py-3 text-gray-700 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700"
+            aria-label="Breadcrumb"
+        >
+            <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+                <li class="inline-flex items-center">
+                    <A
+                        href="/"
+                        class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
+                    >
+                        <svg
+                            class="w-3 h-3 me-2.5"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                        >
+                            <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                        </svg>
+                        Home
+                    </A>
+                </li>
+                {move || {
+                    let path = path.get();
+                    let mut url = String::with_capacity(path.len());
+                    url.push('/');
+                    path.split('/')
+                        .skip(1)
+                        .map(|frag| {
+                            url.push_str(frag);
+                            view! {
+                                <li class="inline-flex items-center">
+                                    <BreadcrumbItem name=frag.to_string() url=url.clone() />
+                                </li>
+                            }
+                        })
+                        .collect_view()
+                }}
+            </ol>
+        </nav>
     }
 }
 
@@ -284,11 +361,11 @@ fn status_row(s: &[StatusRow]) -> impl IntoView {
                         {&first.name}
                     </div>
                 </A>
-                <a target="_blank" href=public_url>
+                <A target="_blank" href=public_url.clone()>
                     <div class="cursor-pointer text-blue-600 underline decoration-gray-800 hover:opacity-80 focus:outline-none focus:opacity-80 dark:decoration-white">
                         "open"
                     </div>
-                </a>
+                </A>
             </td>
             <td>{status_pip_list(s)}</td>
             <td>{first.poll_time.to_string()}</td>
